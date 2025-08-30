edd8a6c600574763ab8d320f323e17fad96add27
MiaTexnes <miatex50428@stud.noroff.no>
2025-08-30 16:50:56 +0200

    net test

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,7 @@
 export const config = {
   X_NOROFF_API_KEY: import.meta.env.VITE_X_NOROFF_API_KEY || "",
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "https://v2.api.noroff.dev",
+  API_BASE_URL:
+    import.meta.env.VITE_API_BASE_URL || "https://v2.api.noroff.dev",
   // Add other config values as needed
 };
 
